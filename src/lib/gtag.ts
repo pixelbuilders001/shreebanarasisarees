2f379b782161313e17b11db9728c09bcc6fbb266
rajeev <rajeev06code>
2026-08-31 13:14:00 +0530

    aas2323234333442323778

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-9FXS37JMYP";
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-Q2T1PB067Q";
 
 declare global {
   interface Window {
